b55ff090f14b7babc855eb704b34d11fe7292c37
Plamen Draganov <plamine95@gmail.com>
2025-12-02 10:36:46 +0200

    Add UserContext usage to App component
    Imported UserContext and useContext in App.jsx and initialized the user variable from context. This prepares the App component to access user data from context.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,8 +6,12 @@ import Home from "./components/home/Home.jsx"
 import Feed from "./components/feed/Feed.jsx"
 import Details from "./components/details/Details.jsx"
 import LandingPage from "./components/landing-page/LandingPage.jsx"
+import UserContext from "./contexts/UserContext.jsx"
+
+import { useContext } from "react"
 
 function App() {
+    const { user } = useContext(UserContext);
 
     return (
         <>
