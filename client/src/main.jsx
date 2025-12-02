08ec2b64e19085f18cec01ee3234e272ed79fa9f
Plamen Draganov <plamine95@gmail.com>
2025-12-02 10:34:05 +0200

    Wrap App with UserProvider context
    App is now wrapped with UserProvider in main.jsx to provide user context throughout the application.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router'
 import { StrictMode } from 'react'
+import { UserProvider } from './contexts/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            <UserProvider>
+                <App />
+            </UserProvider>
         </BrowserRouter>
     </StrictMode>
 )
