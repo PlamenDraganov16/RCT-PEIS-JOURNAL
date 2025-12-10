11f1d62b4c33f234d7822cf27b48995640a4ded0
Plamen Draganov <plamine95@gmail.com>
2025-12-10 12:18:54 +0200

    Add usePersistedState hook and update UserContext
    Introduced a new usePersistedState hook to persist user state in localStorage. Updated UserContext to use this hook for user state management, improving authentication persistence across sessions.

--- client/src/contexts/UserContext.jsx
+++ client/src/contexts/UserContext.jsx
@@ -1,41 +1,44 @@
 import { createContext, useContext, useState } from "react";
-import useRequest from "../hooks/useRequest.js";
+import useRequest from "../hooks/useRequest";
+import usePersistedState from "../hooks/usePersistedState";
 
 const UserContext = createContext({
     isAuthenticated: false,
-
     user: {
         email: '',
         password: '',
         _createdOn: 0,
         _id: '',
-        accessToken: '',
+        accessToken: ''
     },
+    registerHandler() { },
     loginHandler() { },
     logoutHandler() { },
-    registerHandler() { },
-})
+});
 
 export function UserProvider({
     children,
 }) {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = usePersistedState(null);
     const { request } = useRequest();
 
     const registerHandler = async (email, password) => {
         const newUser = { email, password };
 
-        //register api call
+        // Register API call 
         const result = await request('/users/register', 'POST', newUser);
 
-        // login user after register
+        // Login user after register
         setUser(result);
-    }
+    };
 
     const loginHandler = async (email, password) => {
-        const result = await request('/users/login', 'POST', { email, password })
+        const result = await request('/users/login', 'POST', { email, password });
+
+        console.log(result);
+
         setUser(result);
-    }
+    };
 
     const logoutHandler = () => {
         return request('/users/logout', 'GET', null, { accessToken: user.accessToken })
@@ -45,10 +48,11 @@ export function UserProvider({
     const userContextValues = {
         user,
         isAuthenticated: !!user?.accessToken,
+        registerHandler,
         loginHandler,
         logoutHandler,
-        registerHandler
-    }
+    };
+
     return (
         <UserContext.Provider value={userContextValues}>
             {children}
