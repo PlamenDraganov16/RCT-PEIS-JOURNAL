e86ed3d9c03f5da02ad48f1c1a59235f31a36af6
Plamen Draganov <plamine95@gmail.com>
2025-12-10 15:29:51 +0200

    Remove Spinner component
    Deleted the Spinner.jsx file from the components directory. This may be part of a refactor or replacement of the loading indicator.

--- client/src/components/spinner/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-export default function Spinner({ size = 12, color = "green-500" }) {
-  return (
-    <div className="flex justify-center items-center min-h-[20vh]">
-      <div
-        className={`w-${size} h-${size} border-4 border-${color} border-t-transparent rounded-full animate-spin`}
-      ></div>
-    </div>
-  );
-}
